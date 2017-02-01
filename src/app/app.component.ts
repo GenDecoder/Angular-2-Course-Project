f52f336a462d6527847f0a7820ff5b01ba36f9d3
GenDecoder <mariomedranomaldonado@gmail.com>
2017-02-01 01:28:38 -0400

    PC PERSONAL USER TEST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mario Application # 1';
+  title = 'Mario Application # 1 ... ';
   description: string = 'With this app I will become a master';
 }
