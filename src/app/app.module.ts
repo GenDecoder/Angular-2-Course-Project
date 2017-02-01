3c9ab7c3ed30d50f750de93d29e3668b883a2dfa
GenDecoder <mariomedranomaldonado@gmail.com>
2017-02-01 18:12:27 -0400

    Basic set up for structure. (Header, Book).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCardComponent } from './book-list/book-card/book-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookListComponent,
+    BookCardComponent
   ],
   imports: [
     BrowserModule,
